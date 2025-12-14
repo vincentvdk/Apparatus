0a63929df46b03c0e8aadbdee81cd7e981d79626
Vincent Van der Kussen <vincent@vanderkussen.be>
2025-12-14 17:06:47 +0100

    add swayosd

--- os/build_files/build.sh
+++ os/build_files/build.sh
@@ -9,8 +9,8 @@ dnf5 -y copr enable solopasha/hyprland
 dnf5 -y install xdg-desktop-portal-hyprland hyprland hyprland-contrib hyprland-plugins hyprpaper hyprpicker hypridle hyprshot hyprlock pyprland waybar-git xdg-desktop-portal-hyprland hyprland-qtutils
 
 ### -- swayosd
-#dnf5 -y copr enable markupstart/SwayOSD
-#dnf5 -y install swayosd
+dnf5 -y copr enable erikreider/swayosd
+dnf5 -y install swayosd
 
 ## -- Apparatus
 cp /delivery/build_files/apparatus/butler.sh /usr/bin/butler
