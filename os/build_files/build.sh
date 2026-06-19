5e90b0bf693c44c2405bab1a152471b3dc7695b5
Vincent Van der Kussen <vincent@vanderkussen.be>
2026-06-19 14:07:32 +0200

    add hyprland back

--- os/build_files/build.sh
+++ os/build_files/build.sh
@@ -201,7 +201,7 @@ chmod 644 /etc/greetd/*.css /etc/greetd/*.toml
 chmod 755 /etc/greetd
 
 ## -- Disable GDM (from fedora-bootc base image) and enable greetd
-systemctl mask gdm.service
+systemctl mask gdm.service display-manager.service
 systemctl enable greetd.service
 systemctl enable podman.socket
 
