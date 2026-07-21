1d28d5858107b50781170ac4184f9cf037c0d7bc
Vincent Van der Kussen <vincent@vanderkussen.be>
2026-07-21 17:02:50 +0200

    fix: build Hyperland

--- os/build_files/build.sh
+++ os/build_files/build.sh
@@ -69,7 +69,7 @@ dnf5 -y install git cmake gcc-c++ meson ninja-build pkgconf-pkg-config python3 \
     xcb-util-keysyms-devel xcb-util-renderutil-devel \
     pulseaudio-libs-devel pipewire-devel libX11-devel \
     libXext-devel libXfixes-devel libXrandr-devel libXrender-devel \
-    libXinerama-devel libXi-devel
+    libXinerama-devel libXi-devel pugixml-devel
 
 ## -- Build and install hyprwayland-scanner from source (required by aquamarine >= 0.9.3)
 # aquamarine 0.9.3 requires hyprwayland-scanner >= 0.4.0
